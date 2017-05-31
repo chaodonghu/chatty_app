8e9e4dc457a90d7cb31a8875f8d47e5121f7a34a
Dong Hu <cdhu@uwaterloo.ca>
2017-05-31 23:16:21 +0000

    Send messages to server and view message in console

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -8,19 +8,25 @@ const PORT = 3001;
 
 // Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
-  .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+// Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static('public')).listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${PORT}`));
 
 // Create the WebSockets server
-const wss = new SocketServer({ server });
+const wss = new SocketServer({server});
 
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
-wss.on('connection', (ws) => {
+wss.on('connection', (client) => {
   console.log('Client connected');
 
+
+  client.on('message', (rawMessage) => {
+    const message = JSON.parse(rawMessage);
+    // console.log(message);
+    console.log(`User ${message.message.username} said ${message.message.content}`);
+  })
+
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  client.on('close', () => console.log('Client disconnected'));
 });
--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,37 @@ import ChatBar from './ChatBar.jsx';
 // Define application data
 const chattyData = {
   currentUser: {username: "Dong"}, // optional, if currentuser is not defined, it means the user is Anonymous
-  messages: [
-    {
-      id: 1,
-      username: "Dong",
-      content: "Has anyone seen my marbles?"
-    },
-    {
-      id: 2,
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-    }
-  ]
+  messages: [] //messages coming from the server will be stored here as they arrive
 }
 
-content = ''
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = chattyData;
+    this.handleInsertMessage = this.handleInsertMessage.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
+
+    // Connects React app to WebSocket server
+    this.connection = new WebSocket("ws://localhost:3001");
+    this.connection.onopen = (event) => {
+      console.log("We are connected");
+    }
   }
 
-InsertMessage = (message) => {
-    console.log('this is the length of the inputted data', chattyData.messages.length);
+sendMessage(message) {
+  this.connection.send(JSON.stringify(message));
+  console.log('Message sent from client to server');
+}
+
+handleInsertMessage = (message) => {
     let length = this.state.messages.length + 1;
-    console.log(length);
 
     const newMessage = {id: length, username: message.username, content: message.content};
     const messages = this.state.messages.concat(newMessage);
-    console.log(messages);
-    this.setState({messages: messages})
+    console.log(message);
+    this.setState({messages: messages});
+    // Messages going back and forth through the WebSocket connection
+    this.sendMessage({message: message});
   }
 
   // in App.jsx
@@ -43,7 +44,7 @@ InsertMessage = (message) => {
     console.log("componentDidMount <App />");
     // console.log('this is the length of the inputted data', chattyData.messages.length);
     setTimeout(() => {
-      console.log("Simulating incoming message");
+      // console.log("Simulating incoming message");
       // Add a new message to the list of messages in the data store
       const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
       const messages = this.state.messages.concat(newMessage)
@@ -61,7 +62,7 @@ InsertMessage = (message) => {
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
         <MessageList messages={this.state.messages}/>
-        <ChatBar currentUser={this.state.currentUser} InsertMessage={this.InsertMessage}/>
+        <ChatBar currentUser={this.state.currentUser} InsertMessage={this.handleInsertMessage}/>
       </div>
     );
   }
