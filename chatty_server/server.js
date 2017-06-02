7f5d86f1e54500e4d9a4d50d3e26c7fbb51c5dc3
Dong Hu <cdhu@uwaterloo.ca>
2017-06-02 00:27:47 +0000

    Display how many users are online

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -16,50 +16,86 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({server});
 
+// Start user counter at 0
+let counter = 0;
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (client) => {
   console.log('Client connected');
+  _incrementUser();
 
   client.on('message', (rawMessage) => {
     let msg = JSON.parse(rawMessage);
-    switch(msg.type) {
+    switch (msg.type) {
       case "postNotification":
-      let newNotification = {
-        type: "incomingNotification",
-        data: {
-          id: uuid.v1(),
-          prevUserName: msg.data.prevUserName,
-          newUserName: msg.data.newUserName
-        }
-      }
-      wss.clients.forEach((client) => {
-        if(client.readyState === require('ws').OPEN) {
-          client.send(JSON.stringify(newNotification));
-        }
-      })
-      break;
+        _postNotification(msg);
+        break;
       case "postMessage":
-      let newMessage = {
-        type: "incomingMessage",
-        data: {
-          id: uuid.v1(),
-          username: msg.data.username,
-          content: msg.data.content
-        }
-      };
-      wss.clients.forEach((client) => {
-        if(client.readyState === require('ws').OPEN) {
-          client.send(JSON.stringify(newMessage));
-        }
-      });
-      break;
+        _postMessage(msg);
+        break;
       default:
-      console.error("This is an unknown event type: " + serverData.type);
+        console.error("This is an unknown event type: " + msg.type);
     }
   });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  client.on('close', () => console.log('Client disconnected'));
+  client.on('close', () => {
+    console.log('Client disconnected');
+    _decrementUser();
+  });
 });
+
+// ************************ Helper Functions ************************ //
+
+// Broadcast the message to all clients
+_broadcastmsg = (message) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === require('ws').OPEN) {
+      client.send(message);
+    }
+  });
+}
+
+_incrementUser = () => {
+  counter++;
+  let newCount = {
+    type: "incomingCounter",
+    data: counter
+  }
+  _broadcastmsg(JSON.stringify(newCount));
+}
+
+_decrementUser = () => {
+  counter--;
+  let newCount = {
+    type: "incomingCounter",
+    data: counter
+  }
+  _broadcastmsg(JSON.stringify(newCount));
+}
+
+_postNotification = (msg) => {
+  let newNotification = {
+    type: "incomingNotification",
+    data: {
+      id: uuid.v1(),
+      prevUserName: msg.data.prevUserName,
+      newUserName: msg.data.newUserName
+    }
+  }
+  _broadcastmsg(JSON.stringify(newNotification));
+}
+
+_postMessage = (msg) => {
+  let newMessage = {
+    type: "incomingMessage",
+    data: {
+      id: uuid.v1(),
+      username: msg.data.username,
+      content: msg.data.content
+    }
+  };
+  _broadcastmsg(JSON.stringify(newMessage));
+}
